82477ac2c683f1d05c35be55a9fab7cde2ad7a6e
madacoda <hamada.undetected@gmail.com>
2024-04-14 10:35:16 +0700

    step 1 - layout

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,27 @@
+@extends('app.layout')
+
+@section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">User</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">User</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <section class="content">
+            <div class="container-fluid">
+
+            </div>
+        </section>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('user.index');
